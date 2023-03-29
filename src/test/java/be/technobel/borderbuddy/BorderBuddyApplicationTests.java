b5d9a88d0083e29b7cd9ac219b44ba8befdafb45
fdelonville <119604519+fdelonville@users.noreply.github.com>
2023-03-29 17:48:33 +0200

    created entities, dtos and service interfaces + integrated angular

---
+++ src/test/java/be/technobel/borderbuddy/BorderBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technobel.borderbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorderBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
